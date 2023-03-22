c4d05e217117e091bde1f5ab34faaf622c2fc1ca
katab99 <bujdosookata@gmail.com>
2023-03-22 21:06:02 +0100

    Update commit

--- mini project 2/mandulakenyer.py
+++ mini project 2/mandulakenyer.py
@@ -1,12 +1,13 @@
 import numpy as np
 from numba import jit
 import multiprocessing as mp
+import dask.array as da
 
 # re_prop : list of properties
 # re_prop[0] : re_min, re_prop[1] : re_max, re_prop[2] : re_scale
 # im : same as re
 # dt : data type
-def create_imre(re_prop, im_prop, dt):
+def create_imre(re_prop, im_prop, dt=None):
     re = np.linspace(re_prop[0], re_prop[1], re_prop[2], dtype=dt)
     im = np.linspace(im_prop[0], im_prop[1], im_prop[2],  dtype=dt) *1j
     
@@ -49,7 +50,7 @@ def vectorized(re, im, dt, comp_dt, itr, thresh):
     return M
 
 # multiprocessing
-def parallel_mb(c_matrix, dt, itr, thresh):
+def parallel_mb(c_matrix, itr, thresh, dt=None):
     M = np.zeros(c_matrix.shape[0], dtype=dt)
     
     for count, value in enumerate(c_matrix):
@@ -71,11 +72,30 @@ def parallel_mb(c_matrix, dt, itr, thresh):
 def parallelize(func, proc_num, chunk_num, re, im, dt, itr, thresh):
     re_val, im_val = create_imre(re, im, dt)
     comp_matrix = re_val + im_val[:, np.newaxis]
-    items = [(C, dt, itr, thresh) for C in comp_matrix]
+    items = [(C, itr, thresh, dt) for C in comp_matrix]
+
     pool = mp.Pool(processes=proc_num)
     output = [pool.starmap(func, items, chunksize=chunk_num)]
     
     pool.close()
     pool.join()
+    
+    return output
+
+# dask implementation
+def dask_mb(c_matrix, chunk_size, itr, thresh):
+    M = da.zeros(c_matrix.shape, chunks=(chunk_size,chunk_size))
+    z = da.zeros(c_matrix.shape, chunks=(chunk_size,chunk_size))
+
+    for i in range(itr):
+        z = z**2 + c_matrix
+        M[da.abs(z) < thresh] = i
+    
+    return M
+
+def dask_parallel(func, c_matrix, chunk_size, itr, thresh):
+    
+    output = da.map_blocks(func, c_matrix, chunk_size, itr, thresh)
+    output = output.compute()
     
     return output
--- mini project 2/utils.py
+++ mini project 2/utils.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 def add_new_row(method, dt, t):
     if dt == None:
         dt = "default"
@@ -7,3 +9,9 @@ def add_new_row(method, dt, t):
         'dtype' : [dt],
         'time' : [t]
     }
+
+# for visulalizing the results of algorithms
+def plotting(res, title, size_params):
+    plt.figure(figsize=(8,8))
+    plt.title(title)
+    plt.imshow(res, cmap='inferno', extent=[size_params[0], size_params[1], size_params[2], size_params[3]])
