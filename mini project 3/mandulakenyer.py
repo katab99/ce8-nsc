3638dcebe5d506c6d6b334983510247c5de42f64
katab99 <bujdosookata@gmail.com>
2023-04-25 11:48:29 +0200

    Update

--- mini project 3/mandulakenyer.py
+++ mini project 3/mandulakenyer.py
@@ -1,41 +1,7 @@
 '''
 'mandulakenyer' module containts different implementations of Mandelbrot algorithm.
 '''
-
 import numpy as np
-import multiprocessing as mp
-
-"""
-class Mandelbrot():
-
-    def __init__(self, re, im,  I, T):
-        self.re = np.linspace(re[0], re[1], re[2], dtype=np.float32)
-        self.im = np.linspace(im[0], im[1], im[2], dtype=np.float32)
-        self.max_iter = I
-        self.thresh = T
-    
-    def naive():
-        pass
-"""
-
-def create_imre(re_prop, im_prop):
-    '''
-    Returns 2 arrays 
-
-    Inputs
-        re_prop : list of properties of on real-axis - [minimum, maximum, scale]
-        im_prop : list of properties of on imaginary-axis - [minimum, maximum, scale]
-    
-    Outputs
-        re : values on real-axis
-        im : values on imaginary-axis
-    '''
-
-    re = np.linspace(re_prop[0], re_prop[1], re_prop[2], dtype=np.float32)
-    im = np.linspace(im_prop[0], im_prop[1], im_prop[2], dtype=np.float32) *1j
-    
-    return re, im
-
 
 def naive(re, im, itr, thresh):
     '''
@@ -65,8 +31,6 @@ def naive(re, im, itr, thresh):
                 if abs(z) > thresh:
                     M[b, a] = i
                     break
-            #else:
-                #M[b, a] = itr
 
     return M
 
@@ -93,7 +57,5 @@ def vectorized(re, im, itr, thresh):
     for i in range(itr):
         z = z**2 + c
         M[thresh <= abs(z)] = i
-
-    #M[M == 0] = itr
 
     return M
